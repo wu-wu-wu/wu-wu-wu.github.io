30a7254400c05a86bd24c9815c93294c5bb57d4f
wu-wu-wu <wu.wu.wu.world@gmail.com>
2015-12-31 11:12:10 -0500

    added JQuery datepicker fallback to IE form date input

--- samoset-family-dental-website/assets/scripts/script.js
+++ samoset-family-dental-website/assets/scripts/script.js
@@ -1,8 +1,12 @@
 //feature validation and assumed polyfill for date input feature (IE)
 Modernizr.load({
     test: Modernizr.inputtypes.date,
-    nope: "js/jquery-ui.custom.js",//polyfill for nope support for date input
+    nope: "jquery-ui.js",//polyfill for nope support for date input
     callback: function() {
       $("input[type=date]").datepicker();
     }
   });
+ 
+//JQuery datepicket UI element attached to apptdateinput form element
+//$('#apptdateinput').datepicker(); 
+
