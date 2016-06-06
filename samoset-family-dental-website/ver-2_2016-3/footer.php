17ffdce7d9cdde18289d9e94f2fcc24d43387f0f
Suzanne Wu <wu.wu.wu.world@gmail.com>
2016-06-06 09:15:55 -0400

    blog

---
+++ samoset-family-dental-website/ver-2_2016-3/footer.php
@@ -0,0 +1,5 @@
+<?php
+echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+?>
+
+//need to add php footer content
---
+++ samoset-family-dental-website/ver-2_2016-3/nav.php
@@ -0,0 +1,1 @@
+<!--add nav content here in php; figure out button class="selected"-->
