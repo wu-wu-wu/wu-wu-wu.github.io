df6a8afb8d55bd97a7d3c08fff243dd23deb6ef3
wu-wu-wu <wu.wu.wu.world@gmail.com>
2016-10-11 22:38:10 -0400

    moved old wip's

--- wips/README.md
+++
@@ -1,5 +0,0 @@
-
-=======
-# wu-wu-wu.github.io
-test planet
->>>>>>> 26b4cd03dd8b207701c669fd91188ccabb241b50
--- wips/practical-js/3-objects.js
+++
@@ -1,6 +0,0 @@
-{
-  name: 'Gordon',
-  sayName: function() {
-    console.log(this); //this refers to the whole object
-  }
-}
